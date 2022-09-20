cadeb1910a91b06df2dcd627171f451aa0689ed1
Iryna Kramar <irynakramar@Irynas-MacBook-Air.local>
2022-09-20 16:57:09 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
-  $(".bike-slider").slick({
+  $(".bike-slider, .slider__items").slick({
     arrows: false,
+    fade: true,
     dots: true,
     autoplay: true,
     autoplaySpeed: 2000,
